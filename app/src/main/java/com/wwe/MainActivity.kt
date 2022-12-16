d0eb7296bdd9a1a001e316462d9234bd68f14ba5
David B ZHANG <18588744489@163.com>
2022-12-16 08:47:57 +0800

    Update

--- app/src/main/java/com/wwe/MainActivity.kt
+++ app/src/main/java/com/wwe/MainActivity.kt
@@ -1,23 +1,10 @@
 package com.wwe
 
-import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentFactory
 
-
-class MainActivity : AppCompatActivity() {
-
-    private val textViewFragmentCount: TextView by lazy {
-        findViewById(R.id.textViewFragmentCount)
-    }
-
-    private val buttonAddFragment: Button by lazy {
-        findViewById(R.id.buttonAddFragment)
-    }
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     /** 后续 FragmentManager 在创建/恢复 fragment 时，会使用此 factory 创建实例 */
     private val fragmentFactory: FragmentFactory by lazy {
@@ -28,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         // 注意 FragmentFactory 的设置必须在 super.onCreate 之前，因为当 Activity 进入重建路径时，会在 super.onCreate 中使用到它。
         supportFragmentManager.fragmentFactory = fragmentFactory
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction()
             .replace(
                 R.id.fragment_container,
@@ -36,45 +22,5 @@ class MainActivity : AppCompatActivity() {
             )
             .addToBackStack(null)
             .commit()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        Log.i("WWE", "MainActivity onSaveInstanceState")
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        Log.i("WWE", "MainActivity onRestoreInstanceState")
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        when (newConfig.orientation) {
-            Configuration.ORIENTATION_LANDSCAPE -> {
-                Log.i("WWE", "MainActivity onConfigurationChanged landscape")
-            }
-            Configuration.ORIENTATION_PORTRAIT -> {
-                Log.i("WWE", "MainActivity onConfigurationChanged portrait")
-            }
-            else -> {
-                // Do nothing
-            }
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("WWE", "MainActivity onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("WWE", "MainActivity onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("WWE", "MainActivity onDestroy")
     }
 }
